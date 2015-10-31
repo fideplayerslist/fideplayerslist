b5671ed55a2250c3aa06b0f8f8e18731e4cd15f3
fideplayerslist <fideplayerslist@gmail.com>
2015-10-31 19:42:35 +0100

    FIDE

--- players.py
+++ players.py
@@ -1,20 +1,24 @@
 import xml.parsers.expat
 from tkinter import *
 import time
+import os
 
 cnt=0
 state="idle"
 current_key=""
 current_value=""
-fields=[]
+fields={}
 outf=None
 status_label=None
 last_update=time.time()
+key_counts={}
+phase=0
+sorted_keys=[]
 
 def update(message):
 	global cnt
 	global status_label
-	status_label.config(text=message)
+	status_label.config(text="Phase "+str(phase)+" - "+message)
 	status_label.update()
 
 def start_element(name, attrs):
@@ -35,19 +39,32 @@ def end_element(name):
 	global outf
 	global status_label
 	global last_update
+	global phase
+	global key_counts
+	global sorted_keys
 	if state=="player":
 		if name=="player":
 			state="idle"
-			line="\t".join(fields)
-			print(line,file=outf)
-			fields=[]
+			if phase==2:
+				line_list=map(lambda key:fields[key],sorted_keys)
+				print("\t".join(line_list),file=outf)
+			fields={}
 			cnt+=1
 			t=time.time()
 			if (t-last_update)>=1:
 				update("Processed records: "+str(cnt))
 				last_update=t
 		else:
-			fields.append(current_value)
+			fields[current_key]=current_value
+			if phase==1:
+				value=current_value if not current_value=="" else "N/A"
+				if current_key in key_counts:
+					if value in key_counts[current_key]:
+						key_counts[current_key][value]+=1
+					else:
+						key_counts[current_key][value]=1
+				else:
+					key_counts[current_key]={value:1}
 			current_key=""
 			current_value=""
 			
@@ -58,14 +75,12 @@ def char_data(data):
 	if state=="player" and not current_key=="":
 		current_value=data
 
-def process_xml():
-	global outf
+def process_xml_go():
 	global cnt
+	cnt=0
 	p = xml.parsers.expat.ParserCreate()
 
 	fh=open("players_list_xml.xml")
-
-	outf=open("players.txt","w")
 	
 	p.StartElementHandler = start_element
 	p.EndElementHandler = end_element
@@ -79,10 +94,35 @@ def process_xml():
 
 		p.Parse(line)
 
-	outf.close()
-
 	fh.close()
 	update("Processing done, "+str(cnt)+" records processed")
+	
+def process_xml():
+	global phase
+	global outf
+	global sorted_keys
+	phase=1
+	process_xml_go();
+	if not os.path.exists("keycounts"):
+		os.mkdir("keycounts")
+		
+	sorted_keys=list(key_counts.keys())
+	sorted_keys.sort()
+	
+	for key in sorted_keys:
+		print("creating keycount",key)
+		keyf=open("keycounts/"+key+".txt","w")
+		values=list(key_counts[key].keys())
+		values.sort()
+		for value in values:
+			print(value+"\t"+str(key_counts[key][value]),file=keyf)
+		keyf.close();
+		
+	phase=2
+	outf=open("players.txt","w")
+	print("\t".join(sorted_keys),file=outf)
+	process_xml_go();
+	outf.close()
 	
 # mainloop
 	
@@ -91,7 +131,7 @@ root=Tk()
 status_label=Label(root)
 status_label.pack()
 
-process_xml_button = Button(root, text='Process XML', width=25, command=process_xml)
+process_xml_button = Button(root, text='Process XML', width=100, command=process_xml)
 process_xml_button.pack()
 
 root.mainloop()
