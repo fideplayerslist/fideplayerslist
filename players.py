d319fc01dcc5b6de660e6638618d14c1b708ca24
fideplayerslist <fideplayerslist@gmail.com>
2015-11-01 06:37:02 +0100

    FIDE

--- players.py
+++ players.py
@@ -18,6 +18,7 @@ sorted_keys=[]
 def update(message):
 	global cnt
 	global status_label
+	global phase
 	status_label.config(text="Phase "+str(phase)+" - "+message)
 	status_label.update()
 
@@ -97,17 +98,21 @@ def process_xml_go():
 	fh.close()
 	update("Processing done, "+str(cnt)+" records processed")
 	
+def mkdir(path):
+	if not os.path.exists(path):
+		os.mkdir(path)
+	
 def process_xml():
 	global phase
 	global outf
 	global sorted_keys
 	phase=1
 	process_xml_go();
-	if not os.path.exists("keycounts"):
-		os.mkdir("keycounts")
 		
 	sorted_keys=list(key_counts.keys())
 	sorted_keys.sort()
+	
+	mkdir("keycounts")
 	
 	for key in sorted_keys:
 		print("creating keycount",key)
@@ -124,6 +129,54 @@ def process_xml():
 	process_xml_go();
 	outf.close()
 	
+def getkey(key):
+		if not key=="":
+			return key
+		return "NA"
+	
+def iterate_players_txt():
+	global cnt
+	global last_update
+	global phase
+	fh=open("players.txt")
+	headers=fh.readline().rstrip().split("\t")
+	line=True
+	cnt=0
+	collected=("country","birthday")
+	collections={}
+	phase="Iterating players.txt"
+	for key in collected:
+		collections[key]={}
+	while((cnt<1000000) and line):
+		line=fh.readline().rstrip()
+		fields=line.split("\t")
+		if(line):
+			cnt+=1
+			record=dict(zip(headers,fields))
+			for key in collected:
+				subkey=getkey(record[key])
+				if subkey in collections[key]:
+					collections[key][subkey].append(line)
+				else:
+					collections[key][subkey]=[line]
+			t=time.time()
+			if (t-last_update)>=1:
+				update("Processed records: "+str(cnt))
+				last_update=t
+	for key in collected:
+		mkdir(key)
+		for subkey in collections[key]:
+			print("Generating "+key+" : "+subkey)
+			outf=open(key+"/"+subkey+".txt","w")
+			print("\n".join(collections[key][subkey]),file=outf)
+			outf.close()
+			outf=open(key+"/"+subkey+".html","w")
+			print("<table border=1><tr><td>"+"</td><td>".join(headers)+"</td></tr><tr>",file=outf)
+			print("</tr><tr>".join(map(lambda x:("<td>"+"</td><td>".join(x.split("\t"))+"</td>"),collections[key][subkey])),file=outf)
+			print("</tr></table>",file=outf)
+			outf.close()
+	update("Iterating players.txt done , "+str(cnt)+" records processed")
+	
 # mainloop
 	
 root=Tk()
@@ -133,5 +186,8 @@ status_label.pack()
 
 process_xml_button = Button(root, text='Process XML', width=100, command=process_xml)
 process_xml_button.pack()
+
+iterate_txt_button = Button(root, text='Inerate players.txt', width=100, command=iterate_players_txt)
+iterate_txt_button.pack()
 
 root.mainloop()
