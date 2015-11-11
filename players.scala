f108a487d40f733b5017d209d86bed9c4fc06f83
fideplayerslist <fideplayerslist@gmail.com>
2015-11-11 13:34:48 +0100

    FIDE

--- players.scala
+++ players.scala
@@ -4,21 +4,83 @@ import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 import javafx.scene.control.Label
 
+import scala.io.Source
+import scala.xml.pull._
+
 class PlayersClass extends Application {
 
-  override def start(primaryStage: Stage) {
-    primaryStage.setTitle("FIDE Players")
+	def process()
+	{
+	
+		val xml = new XMLEventReader(Source.fromFile("players_list_xml.xml"))
+		
+		def parse(xml: XMLEventReader)
+		{
+		
+			val MAXCNT=1000
+		
+			var cnt=0
+		
+			def loop(currNode: List[String])
+			{
+			
+				if((xml.hasNext) && (cnt<MAXCNT))
+				{
+					xml.next match
+					{
+						case EvElemStart(_, label, _, _) =>
+						
+							println("Element index: " + cnt)
+							cnt=cnt+1
+							
+							println("Start element: " + label)
+							loop(label :: currNode)
+						
+						case EvElemEnd(_, label) =>
+						
+							println("End element: " + label)
+							
+							loop(currNode.tail)
+						
+						case EvText(text) =>
+						
+							println("Text: " + text)
+							loop(currNode)
+						
+						case _ => loop(currNode)
+					}
+				}
+				
+			}
+	
+			loop(List.empty)
+		}
 
-    val root = new StackPane
-    root.getChildren.add(new Label("FIDE players"))
+		parse(xml)
+		
+	}
 
-    primaryStage.setScene(new Scene(root, 300, 300))
-    primaryStage.show()
-  }
+	override def start(primaryStage: Stage)
+	{
+		primaryStage.setTitle("FIDE Players")
+
+		val root = new StackPane
+		root.getChildren.add(new Label("FIDE players"))
+
+		primaryStage.setScene(new Scene(root, 300, 300))
+		primaryStage.show()
+		
+		process()
+	}
+	
 }
 
-object Players {
-  def main(args: Array[String]) {
-    Application.launch(classOf[PlayersClass], args: _*)
-  }
+object Players
+{
+	def main(args: Array[String])
+	{
+		Application.launch(classOf[PlayersClass], args: _*)
+		/*val p=new PlayersClass()
+		p.process()*/
+	}
 }
