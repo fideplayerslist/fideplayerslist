f4e8fdc830116df9af1a38c3115f5279a71e7c62
fideplayerslist <fideplayerslist@gmail.com>
2015-11-12 08:12:09 +0100

    FIDE

--- players.scala
+++ players.scala
@@ -6,23 +6,37 @@ import javafx.scene.control._
 import javafx.scene.input._
 import javafx.event._
 
+import java.io._
 
 import scala.io.Source
 import scala.xml.pull._
 
 class PlayersClass extends Application {
 
+	var phase=1
+
 	def process()
 	{
 	
 		val xml = new XMLEventReader(Source.fromFile("players_list_xml.xml"))
 		
+		var cnt=0
+		
 		def parse(xml: XMLEventReader)
 		{
 		
-			val MAXCNT=1000
-		
-			var cnt=0
+			val MAXCNT=1000000
+			
+			var writer:PrintWriter=null
+			
+			var current_tag=""
+			var current_value=""
+			var current_line:Array[String]=Array[String]()
+			
+			if(phase==1)
+			{
+				writer=new PrintWriter(new File("players.txt"))
+			}
 		
 			def loop(currNode: List[String])
 			{
@@ -33,22 +47,40 @@ class PlayersClass extends Application {
 					{
 						case EvElemStart(_, label, _, _) =>
 						
-							println("Element index: " + cnt)
-							
-							println("Start element: " + label)
+							current_tag=label
+						
 							loop(label :: currNode)
 						
 						case EvElemEnd(_, label) =>
 						
-							println("End element: " + label)
-							
-							cnt=cnt+1
-							
+							if(label=="player")
+							{
+								writer.write(current_line.mkString("\t")+"\n")
+								current_tag=""
+								current_line=Array[String]()
+								current_value=""
+								if((cnt%10000)==0)
+								{
+									println("Element index: " + cnt)
+								}
+								cnt=cnt+1
+							}
+							else
+							{
+								current_line=current_line:+(current_value)
+								current_tag=""
+								current_value=""
+							}
+						
 							loop(currNode.tail)
 						
 						case EvText(text) =>
 						
-							println("Text: " + text)
+							if((text!="")&&(current_tag!=""))
+							{
+								current_value=text
+							}
+						
 							loop(currNode)
 						
 						case _ => loop(currNode)
@@ -59,9 +91,16 @@ class PlayersClass extends Application {
 	
 			loop(List.empty)
 			
+			if(writer!=null)
+			{
+				writer.close()
+			}
+			
 		}
 
 		parse(xml)
+		
+		println("Done. A total of "+cnt+" records processed.")
 		
 	}
 
