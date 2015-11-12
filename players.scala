c8a4ad1fc5b3e31b2d0b2eed50eaf629469c604a
fideplayerslist <fideplayerslist@gmail.com>
2015-11-12 13:58:41 +0100

    FIDE

--- players.scala
+++ players.scala
@@ -1,31 +1,69 @@
-import javafx.application.Application
+import javafx.application._
 import javafx.scene.Scene
 import javafx.scene.layout._
-import javafx.stage.Stage
+import javafx.stage._
 import javafx.scene.control._
 import javafx.scene.input._
 import javafx.event._
+import javafx.geometry._
 
 import java.io._
+import java.lang.System
 
 import scala.io.Source
 import scala.xml.pull._
 
 class PlayersClass extends Application {
 
+	var infoLabel=new Label("")
+	var root=new FlowPane()
+	var modal_root=new FlowPane()
+	var modal_stage=new Stage()
+
+	val MAXCNT=1000000
+	
+	var cnt=0
+
 	var phase=1
+	
+	var abs_t0=0.0
+	
+	def update(info: String)
+	{
+		val elapsed=(System.nanoTime()-abs_t0)/1e9
+		val info_updated=info+" Elapsed %.0f sec , speed %.2f records / sec.".format(elapsed,cnt/elapsed)
+		println(info_updated)
+		Platform.runLater(new Runnable{def run{
+			infoLabel.setText(info_updated)
+		}})
+	}
+	
+	def create_modal()
+	{
+		infoLabel=new Label("")
+		modal_root=new FlowPane()
+		modal_root.setPadding(new Insets(10, 10, 10, 10))
+		modal_root.setStyle("-fx-font-size:18px;")
+		modal_stage=new Stage()
+		modal_stage.initModality(Modality.APPLICATION_MODAL)
+		modal_root.getChildren().add(infoLabel)
+		val modal_scene=new Scene(modal_root,800,50)
+		modal_stage.setScene(modal_scene)
+		modal_stage.showAndWait()
+	}
 
 	def process()
 	{
 	
 		val xml = new XMLEventReader(Source.fromFile("players_list_xml.xml"))
 		
-		var cnt=0
+		var t0=System.nanoTime()
+		abs_t0=System.nanoTime()
+		
+		cnt=0
 		
 		def parse(xml: XMLEventReader)
 		{
-		
-			val MAXCNT=1000000
 			
 			var writer:PrintWriter=null
 			
@@ -38,18 +76,16 @@ class PlayersClass extends Application {
 				writer=new PrintWriter(new File("players.txt"))
 			}
 		
-			def loop(currNode: List[String])
+			def loop()
 			{
 			
-				if((xml.hasNext) && (cnt<MAXCNT))
+				while((xml.hasNext) && (cnt<MAXCNT) && (!interrupted))
 				{
 					xml.next match
 					{
 						case EvElemStart(_, label, _, _) =>
 						
 							current_tag=label
-						
-							loop(label :: currNode)
 						
 						case EvElemEnd(_, label) =>
 						
@@ -59,9 +95,12 @@ class PlayersClass extends Application {
 								current_tag=""
 								current_line=Array[String]()
 								current_value=""
-								if((cnt%10000)==0)
+								val t=System.nanoTime()
+								if(t-t0>5e8)
 								{
-									println("Element index: " + cnt)
+									t0=t
+									val info="Processed: " + cnt + "."
+									update(info)
 								}
 								cnt=cnt+1
 							}
@@ -72,8 +111,6 @@ class PlayersClass extends Application {
 								current_value=""
 							}
 						
-							loop(currNode.tail)
-						
 						case EvText(text) =>
 						
 							if((text!="")&&(current_tag!=""))
@@ -81,15 +118,18 @@ class PlayersClass extends Application {
 								current_value=text
 							}
 						
-							loop(currNode)
-						
-						case _ => loop(currNode)
+						case _ => //loop(currNode)
 					}
+				}
+				
+				if(interrupted)
+				{
+					println("Warning: Processing interrupted.")
 				}
 				
 			}
 	
-			loop(List.empty)
+			loop()
 			
 			if(writer!=null)
 			{
@@ -100,22 +140,33 @@ class PlayersClass extends Application {
 
 		parse(xml)
 		
-		println("Done. A total of "+cnt+" records processed.")
+		update("Done. A total of "+cnt+" records processed.")
 		
+	}
+	
+	var interrupted=false
+	
+	def process_thread()
+	{
+		interrupted=false
+		val t=new Thread(new Runnable{def run{
+			process()
+		}})
+		t.start
+		create_modal()
+		interrupted=true
 	}
 
 	override def start(primaryStage: Stage)
 	{
 		primaryStage.setTitle("FIDE Players")
-
-		val root = new FlowPane
 		
 		val startButton=new Button("Process XML")
 		
 		startButton.setOnAction(new EventHandler[ActionEvent]{
 			override def handle(e: ActionEvent)
 			{
-				process()
+				process_thread()
 			}
 		});
 		
