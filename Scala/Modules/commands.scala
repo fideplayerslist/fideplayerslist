22833ce4a8d84a6df94ba886faac2ef6770f8f3f
fideplayerslist <fideplayerslist@gmail.com>
2015-11-20 11:17:33 +0100

    FIDE

--- Scala/Modules/commands.scala
+++ Scala/Modules/commands.scala
@@ -2,7 +2,7 @@ package commands
 
 import scala.io.StdIn.readLine
 
-import parsexml.ParseXML
+import parsexml._
 
 class CommandInterpreter
 {
@@ -14,16 +14,19 @@ class CommandInterpreter
 	val explanations= Map[String,String](
 		"l" -> "list commands",
 		"xml" -> "parse XML",
+		"ck" -> "count keys",
 		"x" -> "exit"
 		)
 		
 	def listcommands() { result=(for((k,v)<-explanations) yield "%-8s : %s".format(k,explanations(k))).mkString("\n") }
 	def parsexml() { result=new ParseXML("players_list_xml.xml").parse }
+	def count_keys() { result=new CountKeys("players.txt").count }
 	def exit() { finished=true }
 	
 	val commands = Map[String,()=>Unit](
 		"l" -> listcommands,
 		"xml" -> parsexml,
+		"ck" -> count_keys,
 		"x" -> exit
 		)
 	
--- Scala/Modules/parsexml.scala
+++ Scala/Modules/parsexml.scala
@@ -14,10 +14,102 @@ class Timer
 	def elapsed:Double = (System.nanoTime() - t0)/1.0e9
 }
 
-class ParseXML(path: String)
+class HeapSize
+{
+	def heapsize = "heap size "+Runtime.getRuntime().totalMemory()/1000000
+}
+
+class ReadTxtLines(path: String)
+{
+	def lines:Array[String]=
+	{
+		val timer=new Timer
+		println("reading lines of %s , %s".format(path,(new HeapSize).heapsize))
+		val lines=Source.fromFile(path).getLines().toArray
+		println("number of lines %d , elapsed %f , %s".format(lines.length,timer.elapsed,(new HeapSize).heapsize))
+		lines
+	}
+}
+
+class CountKeys(path: String)
 {
 
-	def heapsize = "heap size "+Runtime.getRuntime().totalMemory()/1000000
+	def count():String =
+	{
+
+		val lines=(new ReadTxtLines("players.txt")).lines
+
+		var keycounts=Map[String,Int]()
+
+		var cnt=0
+
+		for(line<-lines)
+		{
+
+			cnt=cnt+1
+
+			val fields=line.split("\t")
+
+			var key:String=""
+			var value:String=""	
+
+			for(field<-fields)
+			{
+				if(key=="")
+				{
+					key=field
+				}
+				else
+				{
+					value=field
+
+					if(keycounts.contains(key))
+					{
+						keycounts+=(key->(keycounts(key)+1))
+					}
+					else
+					{
+						keycounts+=(key->1)
+					}
+
+
+					key=""
+					value=""
+				}
+
+			}
+
+			if((cnt%200000)==0)
+			{
+				println("counted %d , %s".format(cnt,(new HeapSize).heapsize))
+
+				for((k,v)<-keycounts)
+				{
+					println("%-20s : %d".format(k,v))
+				}
+			}
+
+		}
+
+		println("total counted %d , %s".format(cnt,(new HeapSize).heapsize))
+
+		val writer=new PrintWriter(new File("keycounts.txt"))
+
+		for((k,v)<-keycounts)
+		{
+			println("%-20s : %d".format(k,v))
+			writer.write("%s\t%d\n".format(k,v))
+		}
+
+		writer.close
+
+		"Done."
+	}
+
+}
+
+class ParseXML(path: String)
+{
 
 	// maximum number of records to be processed
 	val MAXCNT=1000000
@@ -67,7 +159,7 @@ class ParseXML(path: String)
 						{
 							val elapsed=timer.elapsed
 							val rate=cnt/elapsed
-							println("processed %8d , elapsed %5.0f , rate %5.0f , %s ".format(cnt,elapsed,rate,heapsize))
+							println("processed %8d , elapsed %5.0f , rate %5.0f , %s ".format(cnt,elapsed,rate,(new HeapSize).heapsize))
 						}
 
 					}
